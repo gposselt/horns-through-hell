99c5066e61296fe5923d4711000d8321cc6ff5ec
Gamma_02 <186320072+gposselt@users.noreply.github.com>
2026-01-31 15:40:35 -0800

    Done 5/n level builder assets

--- Assets/Scripts/LevelObjectScripts/DestructibleFloor.cs
+++ Assets/Scripts/LevelObjectScripts/DestructibleFloor.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class DestructibleWall : MonoBehaviour
+public class DestructibleFloor : MonoBehaviour
 {
     [SerializeField]
     private Sprite notBroken, broken;
@@ -10,8 +12,15 @@ public class DestructibleWall : MonoBehaviour
 
     [SerializeField] private BoxCollider2D PlayerCollider;
 
-    private bool destroyed = false; 
+    private Coroutine DestructionDelayCoroutine = null;
+
+    private bool destroyed = false;
+
+    public float destructionDelay = 1.5f, undestructionDelay = 1.5f;
     
+    
+    public bool queueDestruction;
+
     public bool Destroyed
     {
         get => destroyed;
@@ -58,18 +67,50 @@ public class DestructibleWall : MonoBehaviour
         {
             PlayerCollider.enabled = true;
         }
+
+        if (queueDestruction && DestructionDelayCoroutine == null)
+        {
+            DestructionDelayCoroutine = StartCoroutine(DestroyAfterTimeThenReform());
+            queueDestruction = false;
+        }
         
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.transform.CompareTag("PlayerProjectileCollision"))
+        if (other.transform.CompareTag("PlayerCollision") )
         {
             Debug.Log("Player Collision detected!");
-            Destroyed = true;
+
+            if (DestructionDelayCoroutine == null)
+            {
+                //destroy after short time and then reform
+                DestructionDelayCoroutine = StartCoroutine(DestroyAfterTimeThenReform());
+            }
+            else
+            {
+                queueDestruction = true;
+            }
         }
         
         Debug.Log($"Collision detected! {other}");
 
+    }
+ 
+
+    IEnumerator DestroyAfterTimeThenReform()
+    {
+        yield return new WaitForSeconds(destructionDelay);
+        
+        Destroyed = true;
+        
+        yield return new WaitForSeconds(undestructionDelay);
+        
+        Destroyed = false;
+
+        //wait for a little  so that it won't immediatley detect it again
+        yield return new WaitForSeconds(destructionDelay);
+
+        DestructionDelayCoroutine = null;
     }
 }
