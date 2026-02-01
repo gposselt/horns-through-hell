5b74c0fc06f0438d211236e34729faaa858fc64e
Gamma_02 <186320072+gposselt@users.noreply.github.com>
2026-02-01 10:27:21 -0800

    LEVEL EDITOR DONE

---
+++ Assets/Scripts/LevelObjectScripts/SpawnHarpy.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //todo: spawn harpy
+        
+        Destroy(gameObject);
+    }
+
+}
