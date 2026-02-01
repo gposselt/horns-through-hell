c3e4895bdd809c7cebcab639fee86ccb3b33b7d7
Daniel Chen <dtyoakridge@gmail.com>
2026-01-31 16:43:54 -0800

    Bounce blocks done!

---
+++ Assets/Scripts/LevelObjectScripts/BounceBlock.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BounceBlock : MonoBehaviour
+{
+
+    public float bounceVelocity = 10.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If the player collides from the top
+        if (collision.gameObject.CompareTag("PlayerCollision") && collision.GetContact(0).normal.y < -0.5f)
+        {
+            collision.rigidbody.linearVelocityY = bounceVelocity;
+        }
+    }
+}
