392e80153ac4235479ef212849ed585d38c3b587
Daniel Chen <dtyoakridge@gmail.com>
2026-01-31 21:11:59 -0800

    Floor buttons partially done

---
+++ Assets/Scripts/LevelObjectScripts/FloorButton.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(BoxCollider2D))]
+public class FloorButton : MonoBehaviour
+{
+
+    private SpriteRenderer buttonRenderer;
+    private BoxCollider2D boxCollider;
+    public Sprite unpressed;
+    public Sprite pressed;
+
+    public bool isPressed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        buttonRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        buttonRenderer.sprite = unpressed;
+        isPressed = false;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerCollision") && collision.GetContact(0).normal.y < -0.5f)
+        {
+            buttonRenderer.sprite = pressed;
+            isPressed = true;
+            boxCollider.enabled = false;
+            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+        }
+    }
+}
