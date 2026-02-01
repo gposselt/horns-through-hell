92f7f779958f32017f1d5e5e732f58938e7cc0e1
Gamma_02 <186320072+gposselt@users.noreply.github.com>
2026-01-31 21:39:40 -0800

    flame pillar!

---
+++ Assets/Scripts/LevelObjectScripts/Active.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+public class Active : MonoBehaviour
+{
+    public SpriteRenderer activeSprite;
+    public BoxCollider2D boxCollider;
+    
+    //todo: MOVE OVER TO PLAYER, KILLS/DAMAGES PLAYER
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     
+    //     if (other.transform.CompareTag(""))
+    //     {
+    //         other.
+    //     }
+    // }
+}
