d5be961b5eb335c40a36d47f595691341ad6dab4
Gamma_02 <186320072+gposselt@users.noreply.github.com>
2026-02-04 16:58:13 -0800

    Feature finished

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -3,5 +3,6 @@ public class Constants
 {
     public static readonly float TimeEpsilon = 0.0001f;
     public static readonly float PhysEpsilon = 0.01f;
+    public static readonly float RenderEpsilon = 0.00001f;
 
 }
